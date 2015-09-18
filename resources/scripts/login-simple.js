fb6f6e6793b7e0b17f87fdf38be4ea8d7370d749
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-09-18 15:04:42 +0530

    Fixed the hide issue in case box is visible.

--- resources/scripts/login-simple.js
+++ resources/scripts/login-simple.js
@@ -2,14 +2,28 @@ jQuery( '#box-signup-show' ).click( function( e ) {
 
 	e.preventDefault();
 
-	jQuery( '#box-login' ).slideUp( 'fast' );
-	jQuery( '#box-signup' ).slideDown( 'slow' );
+	if( jQuery( '#box-signup' ).is( ":visible" ) ) {
+
+		jQuery( '#box-signup' ).slideUp( 'fast' );
+	}
+	else {
+
+		jQuery( '#box-login' ).slideUp( 'fast' );
+		jQuery( '#box-signup' ).slideDown( 'slow' );
+	}
 });
 
 jQuery( '#box-login-show' ).click( function( e ) {
 
 	e.preventDefault();
 
-	jQuery( '#box-signup' ).slideUp( 'fast' );
-	jQuery( '#box-login' ).slideDown( 'slow' );
+	if( jQuery( '#box-login' ).is( ":visible" ) ) {
+
+		jQuery( '#box-login' ).slideUp( 'fast' );
+	}
+	else {
+
+		jQuery( '#box-signup' ).slideUp( 'fast' );
+		jQuery( '#box-login' ).slideDown( 'slow' );
+	}
 });
